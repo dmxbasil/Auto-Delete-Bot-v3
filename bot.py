e1ef64b22fe10fb91f6676215ad20073520c0e38
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2023-03-11 20:31:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ ADMINS = []
 for usr in environ.get("ADMINS").split():
     ADMINS.append(int(usr))
 
-START_MSG = "<b>Hai {},\nI'm a private bot of @mh_world to delete group messages after a specific time</b>"
+START_MSG = "<b>ʜᴀɪ {},\n ɪᴀᴍ ᴀ ᴩʀɪᴠᴀᴛᴇ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ꜰᴏʀ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇꜱꜱᴀɢᴇꜱ ɪɴ ᴄʟ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴩ</b>"
 
 
 User = Client(name="user-account",
